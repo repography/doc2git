0ab91801c6549d6a70f58f40b5761c52f3f4d867
Arpad Ray <arraypad@gmail.com>
2023-03-03 16:01:07 +0100

    chore: remove unused helper

--- src/__tests__/__helpers__/testing-library-react/index.tsx
+++ src/__tests__/__helpers__/testing-library-react/index.tsx
@@ -1,21 +1,14 @@
 import { render, RenderOptions } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import type { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
-import React, { FC, ReactElement } from 'react';
-
-/**
- * Add providers here for unit tests.
- */
-const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
-	return <>{children}</>;
-};
+import { ReactElement } from 'react';
 
 const customRender = (
 	ui: ReactElement,
 	options?: Omit<RenderOptions, 'wrapper'>,
 ): ReturnType<typeof render> & { user: UserEvent } => ({
 	user: userEvent.setup(),
-	...render(ui, { wrapper: AllTheProviders, ...options }),
+	...render(ui, options),
 });
 
 export * from '@testing-library/react';
